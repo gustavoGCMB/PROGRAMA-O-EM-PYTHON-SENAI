a5089503254d3c991191e542297be22efc818f9b
gustavoGCMB <gustavocardozao10@gmail.com>
2025-05-22 17:44:36 -0300

    Add files via upload

---
+++ atividade2.py
@@ -0,0 +1,8 @@
+#soma de 2
+n1= float(input('numero 1:'))
+n2= float(input('numero 2:'))
+calculo= n1+n2
+print('resultado soma',n1+n2)
+print('resultado subtração',n1-n2)
+print('resultado multiplicação',n1*n2)
+print('resultado divisão',n1/n2)
