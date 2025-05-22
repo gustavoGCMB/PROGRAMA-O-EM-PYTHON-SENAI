cd48e22ffa0146b61aec279b47b51d46911b3f02
gustavoGCMB <gustavocardozao10@gmail.com>
2025-05-22 17:46:45 -0300

    Add files via upload

---
+++ atividade 1.py
@@ -0,0 +1,37 @@
+#COMENTÁRIO
+#DADOS PRIMITIVOS
+#PARA CARREGAR O CÓDIGO, CLIQUE EM F5
+#STRINGS  = TEXTOS
+#função print, mostra, textos na tela
+print('OLÁ MUNDO!')
+print('sejam bem vindos', 'pessoas')
+print('ola', ' meu nome é', 'kevin')
+print('----------')
+
+#dado do tipo inteiro(int)
+print(1)
+print(-30)
+print(0)
+#dado do tipo real (float)
+print(1.5)
+print(10.0)
+print(30.5)
+print(0.)
+#dado do tipo boleano
+print(True)
+print(False)
+#input()= guardar um dado
+print(input('digite seu nome:'))
+#variáveis:
+nome= 'Gustavo'
+idade= 15
+endereço= 'rua mandirituba, 105'
+curso= 'programação em python'
+print('ola meu nome é ',nome)
+print('minha idade é', idade)
+print('minha casa esta no endereço', endereço)
+print('faço o curso', curso)
+#calculadora de soma
+numero_1= int(input('digite um número'))
+numero_2= int(input('outro número'))
+print(numero_1+ numero_2)
