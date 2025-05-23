aa31a09cd515d0d7dfad9c141fcba57a3aa59ce2
gustavoGCMB <gustavocardozao10@gmail.com>
2025-05-23 16:34:50 -0300

    Add files via upload

---
+++ atividade4.py
@@ -0,0 +1,18 @@
+#variaveis
+
+#espaços na memoria do pc
+#como criar uma variavel?
+nome= 'marcos'
+
+
+n1 = float(input('numero: '))
+n2 = float(input('numero: '))
+print(n1+n2)
+
+#4 tipos primitivos de dados
+
+real=4.0
+inteiro=10
+textos='testes'
+boleano=True
+
