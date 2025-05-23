865d0a90e3a56e10fc245740b50fa589fb5db1b5
gustavoGCMB <gustavocardozao10@gmail.com>
2025-05-23 17:54:03 -0300

    Add files via upload

---
+++ atividade5.py
@@ -0,0 +1,36 @@
+## 1 - Crie um programa para efetuar a leitura de 
+# um número inteiro e apresentar o resultado do 
+# quadrado deste número.
+n1= float(input('numero: '))
+n2= float(input('numero: '))
+print(n1+n2)
+## 2 - Crie duas variáveis para armazenar 
+# seu primeiro nome e sobrenome. Em seguida, 
+# concatene-as para formar seu nome completo 
+# e exiba o resultado.
+nome = 'Gustavo'
+sobrenome = 'Cardozo'
+print(nome,sobrenome)
+## 3 - Peça ao usuário para digitar dois 
+# números inteiros e armazene-os em variáveis. 
+# Realize a concatenação desses números como 
+# strings e exiba o resultado.
+numero_um = float(input('digite um numero: '))
+numero_dois = float(input('digite um numero: '))
+resultado= (numero_um+numero_dois)
+print(numero_um+numero_dois)
+    
+      
+## 4 - Crie uma variável para armazenar
+# a palavra "Python". Em seguida, adicione 
+# um número inteiro ao final da palavra usando 
+# a concatenação e exiba o resultado.
+
+palavra= 'python'
+print(palavra,10)
+## 5 - Declare uma variável contendo uma 
+# frase. Em seguida, peça ao usuário para 
+# digitar uma palavra e concatene essa 
+# palavra no final da frase. Exiba o resultado.
+frase='qual seu nome?'
+input(frase)
---
+++ atividade6.py
@@ -0,0 +1,9 @@
+#concartenar ->
+
+#1
+nome='julio'
+print('bom dia,',nome)
+print('bom dia,'+' ' + nome)
+print(f'bom dia, {nome}')
+print('bom dia,{}'.format(nome))
+print('bom dia, %s'%(nome))
