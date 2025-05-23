370f5428769c01423ac0540a22916fcc2f8f9ba5
gustavoGCMB <gustavocardozao10@gmail.com>
2025-05-23 16:28:23 -0300

    Add files via upload

---
+++ operadores_aritmeticos.py
@@ -0,0 +1,25 @@
+# +   mais
+
+print('soma',10+2)
+
+# -   menos
+
+print('subtração',10-2)
+
+# *   multiplicação
+
+print('multiplicação', 10*2)
+
+# /   divisão
+
+print('divisão',10/2)
+
+# **  exponenciação
+
+print('expoente',10**2)
+
+# //  divisão com 2  barras
+
+
+print('div 2 c/ barras', 15//2)
+
