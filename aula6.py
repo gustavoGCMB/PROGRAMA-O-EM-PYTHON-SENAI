79b6d3a80597431920907a825bab9d82eb422dbc
gustavoGCMB <gustavocardozao10@gmail.com>
2025-05-28 17:18:09 -0300

    Add files via upload

---
+++ aula6.py
@@ -0,0 +1,13 @@
+palavra='isso é um python'
+comprimento=len(palavra)
+print(comprimento)
+lista=[2,1,3,4]
+print(lista)
+lista=[38000]
+print(lista)
+lista=list(palavra)
+print(lista)
+tupla=('tr,'ana','Pedro')
+print(tupla)
+#range()
+lista=list(range(10,201,10))
