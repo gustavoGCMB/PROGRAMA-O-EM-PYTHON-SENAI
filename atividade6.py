60a45b54575838ab328ba443aa9e5a5c14624ca0
gustavoGCMB <gustavocardozao10@gmail.com>
2025-05-28 15:33:39 -0300

    Add files via upload

--- atividade6.py
+++ atividade6.py
@@ -1,9 +1,39 @@
-#concartenar ->
+#indentação em python-organizar o codigo py
+#tipo de organização do código
+#variável precisa estar dentro da borda
+#quando não estiver dentro de nenhum escopo
+x=10
+#quando estiver dentro de um escopo
+#precisar ter pelo menos um espaço
+#o padrão são quatro espaços
+if x==10:
+    print(x)
+    
 
-#1
+variável='teste'
+x=10
+for n in range(1,3):
+if variável=='teste':
+   print('ok')
+else:
+   print('teste2')
+if 1>10:
+    x:10
+#concatenação=juntar informações
+nome=maria
+print('ola,seja bem vinda',nome)
+print('ola,seja bem vinda'+' '+ nome)
+print('ola,seja bem vinda {}'format(nome))
+print('ola,seja bem vinda %s'%(nome))
+print(f'ola,seja bem vinda {nome}')
+print(f'ola,seja bem vinda\n',nome)
+#-------------------------------------
+#refatorar = melhorar o código
+#encontrar erros
 nome='julio'
-print('bom dia,',nome)
-print('bom dia,'+' ' + nome)
-print(f'bom dia, {nome}')
-print('bom dia,{}'.format(nome))
-print('bom dia, %s'%(nome))
+sobrenome='cesar'
+print('hello',nome,sobrenome)
+#exemplo incorreto
+x='julio'
+y='cesar'
+print('hello',x,y)
---
+++ exerciciosaula6.py
@@ -0,0 +1,35 @@
+#VAMOS PRATICAR COM VARIÁVEIS:
+#Multiplique 4 por 6 e divida o resultado por 2. (Concatene com a palavra resultado)
+n1=4
+n2=6
+n3=2
+calculo=(n1*n2/n3)
+print('resultado =',calculo)
+#Calcule a soma de 5 e 7.(Concatene com a palavra resultado)
+n1=7
+n2=5
+calculado=(5+7)
+print('resultado =',calculo)
+#Eleve 3 à potência de 4.(Concatene com a palavra resultado)
+n1=3
+n2=4
+calculo=(3**4)
+print('resultado =',calculo)
+#Calcule a média de 8, 12 e 15.(Concatene com a palavra resultado)
+n1=8
+n2=12
+n3=15
+calculo=(8 + 12 + 15)/3
+print('resultado =',calculo)
+#Subtraia 10 de 2 e multiplique o resultado por 5.(Concatene com a palavra resultado)
+n1=10
+n2=2
+n3=5
+calculo=(10 - 2)* 5
+print('resultado =',calculo)
+#Divida 27 por 3 e some 5.(Concatene com a palavra resultado)
+n1=27
+n2=3
+n3=5
+calculo=(27/3) + 5
+print('resultado =',calculo)
